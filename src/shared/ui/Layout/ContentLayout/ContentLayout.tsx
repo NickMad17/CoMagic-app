a0983277678fdbb265d71427bacdd352684a0c82
Maady <vlad030911@gmail.com>
2024-03-13 18:06:45 +0300

    change UsersList.tsx

--- src/shared/ui/Layout/ContentLayout/ContentLayout.tsx
+++ src/shared/ui/Layout/ContentLayout/ContentLayout.tsx
@@ -9,7 +9,7 @@ export const ContentLayout = ({children}: Props) => {
 
 
     return (
-        <Layout className='px-10 h-[100vh]'>
+        <Layout className='scrollbar px-10 h-[100vh] overflow-x-auto'>
             {children}
         </Layout>
     );
